577686d0b436234c8dd0aa54c8409146fac9c693
Lukasz <34094307+devluke88@users.noreply.github.com>
2020-04-13 22:51:18 +0100

    Add files via upload

---
+++ ch9_913.py
@@ -0,0 +1,18 @@
+from collections import OrderedDict
+
+glossary0 = OrderedDict()
+
+glossary0['variable'] = 'name that holds the value',
+glossary0['if statement'] = 'condition that is checked before program run some command',
+glossary0['for_loop'] = 'loop that run until is finished or condition is met',
+glossary0['while_loop'] = 'loop that starts when condition is met and stops as well when met',
+glossary0['lista'] = 'values stored in squared bracets',
+glossary0['test'] = 'test3 value',
+glossary0['test2'] = 'test2 value',
+glossary0['test3'] = 'test wvalue',
+glossary0['test4'] = 'teste value',
+glossary0['test5'] = 'testws value',
+
+
+for k, v in glossary0.items():
+    print("Favourite programming word: %s\n\t%s" % (k,v))
