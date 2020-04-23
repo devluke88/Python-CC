f82e514b4937869a84734f24e8e8e460ea18c750
Lukasz <34094307+devluke88@users.noreply.github.com>
2020-04-23 20:06:34 +0100

    Add files via upload

--- city_functions.py
+++ city_functions.py
@@ -1,2 +1,6 @@
-def function_cc(city, country):
-    return f"{city.title()}, {country.title()}"
+def function_cc(city, country, population=""):
+    if population:
+        city_country = city.title() + ", " + country.title() + " - population " + str(population)
+    else:
+        city_country = city.title() + ", " + country.title() 
+    return city_country
